5cdc9e6550f4e433410eb5d6cf47866631f577bf
Twan <twan.hoogveld@student.hu.nl>
2019-12-31 00:15:20 +0100

    live role

--- main.py
+++ main.py
@@ -10,6 +10,38 @@ import textwrap
 #prefix
 Client = Bot('!')
 
+class MyClient(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args,**kwargs)
+        self.bg_task = self.loop.create_task(self.checkForStreaming())
+
+    async def checkForStreaming(self):
+        await self.wait_until_ready()
+        #get the streamer role from the guild, by id.
+        guild = self.get_guild(491609268567408641)
+        streamerRole = guild.get_role(659062011082047499)
+        liveRole = guild.get_role(660090844124282881)
+        #Test channel to send notifcation of live, by id. Later this should add the Role "LIVE".
+        channel = self.get_channel(660083659801493505)
+        await channel.send("Bot is running, this is a 1 minute interval message to see if it is still online.")
+        while not self.is_closed():
+            #all users with the streamer role
+            for member in streamerRole.members:
+                for activity in member.activities:
+                    if activity.name == "Twitch":
+                        if liveRole not in member.roles:
+                            await channel.send(member.name + "is live --- TEST")
+                            #await member.add_roles(liveRole)
+            #Checking if still live.
+            for member in liveRole.members:
+                activList = []
+                for activity in member.activities:
+                    activList.append(activity.name)
+                if "Twitch" not in activList:
+                    await channel.send(member.name + "is no longer live --- TEST")
+                    #await member.remove_roles(liveRole)
+            await asyncio.sleep(30)
+
 #command clear
 @Client.command(pass_context=True)
 @has_any_role(627925171818332180, 512365666611757076, 652607412611710984)
@@ -194,6 +226,8 @@ def makeWelcomeBanner(name):
 
 #run bot
 token = getToken("config.txt", "TOKEN")
+client = MyClient()
+client.run(token)
 Client.run(token)
 
 """""""""
